de8d6071aabf96d283c8109d3bd2d6a3fa116485
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2022-08-01 11:08:37 +0500

    almost completed

--- Ejs challenge/app.js
+++ Ejs challenge/app.js
@@ -3,24 +3,63 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const _ = require('lodash')
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const homeStartingContent = "Hundreds of firefighters in California are battling the largest wildfire to spread in the state so far this year.The McKinney Fire, which started in the northern Siskiyou county on Friday, has already burnt 21,000 hectares (52,500 acres), the state's fire service said.At least 2,000 residents as well as trekkers on the Pacific Crest hiking trail have left the area, authorities say. Homes have been destroyed.It was 0% contained as of Sunday, the emergency service's latest report said.A red flag warning indicating the threat of dangerous fire conditions is in place, as California suffers from persistent drought conditions.A state of emergency was declared in Siskiyou county on Saturday, after homes were destroyed and infrastructure was threatened, state governor Gavin Newsom said.The fire was intensified and spread by dry fuels, extreme drought conditions, high temperatures, winds and lightning storms";
+const aboutContent = "Hi, I’m M Hashim khan and I am a Full-stack Web Developer. It's been 1 years, I’m working as a full-stack web developer. I have developed many websites.";
+const contactContent = "I will add my contact as soon as possible here.";
 
 const app = express();
 
 app.set('view engine', 'ejs');
 
+let posts = []
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-app.get('/' , (req , res) => {
-  
+app.get("/" , (req , res) => {
+  res.render('home', {homePara : homeStartingContent , post : posts})
+  // console.log(posts)
+
 })
 
+app.get("/about" , (req , res) => {
+  res.render('about' , {aboutText : aboutContent})
+})
+app.get("/contact" , (req , res) => {
+  res.render('contact' , {contactText : contactContent})
+})
 
+app.get("/compose" , (req , res) => {
+  res.render('compose')
 
+})
+
+app.post("/compose" , (req , res) => {
+  const postData = {
+    title : req.body.postTitile,
+    content : req.body.PostBody
+  }
+  posts.push(postData)
+
+  res.redirect('/')
+
+})
+
+app.get("/post/:blogname" , (req , res) => {
+  const requestedTitle = _.lowerCase(req.params.blogname);
+  
+  posts.forEach((post) => {
+    let storedTitle = _.lowerCase(post.title)
+
+    if(storedTitle === requestedTitle){
+      console.log("match found")
+      res.render("post" , {title : post.title , content : post.content })
+      // res.redirect('/post')
+    }
+  })
+})
 
 
 
@@ -31,3 +70,17 @@ app.get('/' , (req , res) => {
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+
+
+
+// <!-- <% for(let i = 0; i < homepara.length; i++){ %>
+  // <p> <%= homePara[i] %> </p>
+// <% } %>
+
+
+// <% post.map((data) => { %>
+//   <p><%= data.title</p> %>
+//   <p><%= data.content</p> %>
+
+// <% }) %>
